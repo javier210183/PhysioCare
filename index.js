a5c3dac3977acea0d684357b4ecf51a1d9bb9ee2
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2025-02-06 23:55:48 +0100

    RETURN

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
 
 // Conexión a MongoDB y arranque del servidor
 const PORT = process.env.PORT || 8080;
-mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/physiocare', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
